c52b3216243cd08e8131dd01bf150ae7e3acef53
volveezz <beconc@bk.ru>
2022-08-12 09:28:01 +0900

    fix #6

--- events/tzEvent.js
+++ events/tzEvent.js
@@ -26,6 +26,6 @@ exports.default = {
         if (member) {
             !member.permissions.has("Administrator") ? member.setNickname(`[+${tz}] ${member.displayName}`) : [];
         }
-        interaction.update({ embeds: [embed], components: [] });
+        interaction.editReply({ embeds: [embed], components: [] });
     }),
 };
