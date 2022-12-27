90b8d611aa64797a79f40f6deb27eea0047ebee1
volveezz <beconc@bk.ru>
2022-12-27 11:06:00 +0900

    Process exit patch

--- release/index.js
+++ release/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import { ExtendedClient } from "./structures/client.js";
 import { resolve, join } from "path";
 import "dotenv/config";
-import fetch from "node-fetch";
 export const client = new ExtendedClient();
 await client.start();
 client.rest.on("rateLimited", (rateLimit) => {
@@ -33,17 +32,7 @@ const date = new Date();
 date.setHours(23, 0, 0, 0);
 if (date.getTime() - new Date().getTime() < 0)
     date.setDate(date.getDate() + 1);
-setTimeout(() => {
-    fetch(`https://api.heroku.com/apps/djsn9/dynos/web.1/actions/restart`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${process.env.HEROKU_API_KEY}`,
-        },
-    }).then((result) => {
-        console.log(`Dyno is restarting`, { result });
-    });
-}, date.getTime() - new Date().getTime());
+setTimeout(() => process.exit(0), date.getTime() - new Date().getTime());
 app.get("/", async (req, res) => {
     if (req.query.code && req.query.code.length > 20 && req.query.state && req.query.state.length > 20) {
         const { default: webHandler } = await import("./functions/webHandler.js");
