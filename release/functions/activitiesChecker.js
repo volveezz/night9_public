20cdfe0bee3a84fbcb281bda094a58e6e013ce65
volveezz <beconc@bk.ru>
2023-02-25 04:09:25 +0100

    2.1.10

--- release/functions/activitiesChecker.js
+++ release/functions/activitiesChecker.js
@@ -25,7 +25,7 @@ export async function destinyActivityChecker(authData, member, mode, count = 250
             await checker();
             async function checker() {
                 const response = await fetchRequest(`Platform/Destiny2/${platform}/Account/${bungieId}/Character/${character}/Stats/Activities/?count=${count}&mode=${mode}&page=${page}`, accessToken);
-                if (!response || !response.activities)
+                if (!response)
                     return console.error(`[Error code: 1018] Response error for ${bungieId} during checking ${mode} mode`);
                 if (response.activities?.length > 0) {
                     await Promise.all(response.activities.map((activity) => {
