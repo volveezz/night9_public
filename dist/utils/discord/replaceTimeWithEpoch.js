6994d266bad5652f5a21e29fb6982770e7503fa8
Volve <beconich@gmail.com>
2023-04-17 16:15:13 +0200

    2.9.0.5

--- dist/utils/discord/replaceTimeWithEpoch.js
+++ dist/utils/discord/replaceTimeWithEpoch.js
@@ -1,29 +1,39 @@
 import { EmbedBuilder } from "discord.js";
-import colors from "../../configs/colors.js";
 import { channelIds } from "../../configs/ids.js";
 import { client } from "../../index.js";
+import { timer } from "../general/utilities.js";
 const publicNewsChannel = client.getCachedTextChannel(channelIds.externalNewsFeed) || (await client.getCachedGuild().channels.fetch(channelIds.externalNewsFeed));
 async function handleTimeReplacement({ message: oldMessage }) {
-    const message = await oldMessage.fetch();
-    const messageEmbed = message.embeds[0];
-    if (!messageEmbed || !messageEmbed.description)
+    const fetchMessage = async () => {
+        let message = await oldMessage.fetch();
+        const messageEmbed = message.embeds[0];
+        return messageEmbed ? message : null;
+    };
+    let message = await fetchMessage();
+    if (!message) {
+        await timer(1000 * 10);
+        message = await fetchMessage();
+    }
+    if (!message || !message.embeds[0].description == null)
         return;
+    const messageEmbed = message.embeds[0];
     const messageDescription = messageEmbed.description;
     const replacedDescription = replaceTimeWithEpoch(messageDescription);
     if (!replacedDescription || replacedDescription.length < 5) {
         console.error(`[Error code: 1699]`, message);
         return;
     }
-    const embed = new EmbedBuilder()
-        .setColor(colors.serious)
-        .setDescription(replacedDescription)
-        .setFooter({ text: `BungieHelp notification` });
+    const embed = new EmbedBuilder().setColor("#FFA500").setDescription(replacedDescription).setFooter({ text: `BungieHelp notification` });
     if (messageEmbed.author)
         embed.setAuthor(messageEmbed.author);
     if (messageEmbed.title)
         embed.setTitle(messageEmbed.title);
     if (messageEmbed.timestamp)
         embed.setTimestamp(new Date(messageEmbed.timestamp));
+    if (messageEmbed.image)
+        embed.setImage(messageEmbed.image.url);
+    if (messageEmbed.thumbnail)
+        embed.setImage(messageEmbed.thumbnail.url);
     await publicNewsChannel.send({ embeds: [embed] });
     await message.delete();
 }
@@ -55,7 +65,7 @@ function replaceTimeWithEpoch(text) {
         month = monthNames.indexOf(monthStr);
         setDate = true;
     }
-    const timeRegex = /(\d{1,2})(:)?(\d{2})?\s?(AM|PM)(\s?PDT\s?\(-7\s?UTC\))?/gi;
+    const timeRegex = /(\d{1,2})(:)?(\d{2})?\s?(AM|PM)(\s?PDT\s?\(-7\s?UTC\)|\s?PST\s?\(\d{4}\s?UTC\)|\s?PST\s?\(-7\s?UTC\)|\s?PT|\s?PDT|\s?PST\s?\(-8\s?UTC\)|\s?PST)?/gi;
     const replacement = (match, hour, colon, minute, amPm, timezone) => {
         let hourNumber = parseInt(hour, 10);
         const minuteNumber = parseInt(minute || "0", 10);
@@ -65,7 +75,12 @@ function replaceTimeWithEpoch(text) {
         else if (amPm.toUpperCase() === "AM" && hourNumber === 12) {
             hourNumber = 0;
         }
-        hourNumber += 7;
+        if (timezone && timezone.includes("PST")) {
+            hourNumber += 8;
+        }
+        else {
+            hourNumber += 7;
+        }
         const date = new Date();
         if (setDate) {
             date.setFullYear(date.getFullYear(), month, day);
