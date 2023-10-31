f35b5dc6bfa826d125be6f1081ec4cdabf319d1f
Volve <beconich@gmail.com>
2023-10-31 10:43:46 +0100

    2.21.14.1

--- dist/utils/general/raidFunctions/raidFireteamChecker/raidFireteamChecker.js
+++ dist/utils/general/raidFunctions/raidFireteamChecker/raidFireteamChecker.js
@@ -9,9 +9,14 @@ export function stopFireteamCheckingSystem(raidId) {
     if (raidId) {
         const notifyTimeout = notifyInitializerTimeoutMap.get(raidId);
         const notifyInterval = notifyIntervalMap.get(raidId);
-        notifyInitializerTimeoutMap.delete(raidId) && clearTimeout(notifyTimeout);
+        const isCheckerTimeoutExisted = notifyInitializerTimeoutMap.delete(raidId);
+        if (isCheckerTimeoutExisted) {
+            clearTimeout(notifyTimeout);
+        }
+        if (fireteamCheckingSystem.delete(raidId) && !isCheckerTimeoutExisted) {
+            updateFireteamNotification(raidId, true);
+        }
         notifyIntervalMap.delete(raidId) && clearInterval(notifyInterval);
-        fireteamCheckingSystem.delete(raidId) && updateFireteamNotification(raidId, true);
     }
     else {
         notifyInitializerTimeoutMap.forEach((timeout) => clearTimeout(timeout));
@@ -22,8 +27,6 @@ export function stopFireteamCheckingSystem(raidId) {
     }
 }
 async function raidFireteamCheckerSystem(raidId) {
-    if (process.env.NODE_ENV === "development")
-        return;
     console.debug("Initializing fireteam checker", raidId);
     stopFireteamCheckingSystem(raidId);
     const ongoingRaids = await fetchOngoingRaids(raidId);
@@ -43,6 +46,7 @@ async function raidFireteamChecker(raidParam) {
     const raidStartTimePlus5 = new Date(startTime + 1000 * 60 * 5).getTime();
     console.debug(`Next step for raid ID: ${raidId} will be in ${(raidStartTimePlus5 - Date.now()) / 1000}s`);
     const timeout = setTimeout(async () => {
+        notifyInitializerTimeoutMap.delete(raidId);
         console.debug("Processing with the next step for raid ID", raidId);
         try {
             console.debug("Trying to send a private channel notification for raid ID:", raidId);
