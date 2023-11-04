9ba5182d8486297eb5fdcde01835b7595d825ff9
Volve <beconich@gmail.com>
2023-11-04 02:23:07 +0100

    Redis error update

--- dist/utils/persistence/redis.js
+++ dist/utils/persistence/redis.js
@@ -1,9 +1,15 @@
 import { createClient } from "redis";
+if (process.env.REDIS_PRIVATE_URL) {
+    console.debug("Connecting to redis with the private url");
+}
+else {
+    console.debug("Connecting to redis with the usual url");
+}
 const redisClient = createClient({
     url: process.env.REDIS_PRIVATE_URL || process.env.REDIS_URL,
 });
 redisClient.on("error", (error) => {
-    console.error(`Redis error: ${error}`);
+    console.error("Redis error", error);
 });
 redisClient.connect();
 export { redisClient };
