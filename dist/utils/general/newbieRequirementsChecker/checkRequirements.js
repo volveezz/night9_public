ad42c23122a9cdc7e671ef0e0c57433b23f8b6d5
Volve <beconich@gmail.com>
2024-11-30 02:54:43 +0900

    Formatting fix

--- dist/utils/general/newbieRequirementsChecker/checkRequirements.js
+++ dist/utils/general/newbieRequirementsChecker/checkRequirements.js
@@ -4,7 +4,7 @@ const checkRequirements = (userProfile) => {
     let allRequirementsMet = true;
     const userActiveScore = userProfile.profileRecords.data.activeScore;
     if (userActiveScore < minActiveScore) {
-        message += `Вы не подходите по следующим требованиям: Минимальный счёт активных триумфов (минимум ${minActiveScore}, у вас: ${userActiveScore})\n`;
+        message += `Вы не подходите по следующим требованиям:\n - Минимальный счёт активных триумфов (минимум ${minActiveScore}, у вас: ${userActiveScore})\n`;
         allRequirementsMet = false;
     }
     return { allRequirementsMet, message };
